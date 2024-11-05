e930f1f50d7f163dd650316c1e872b7661d940ff
WistonTravieso <wistontravieso@gmail.com>
2024-11-05 01:03:29 +0000

    [Proyecto terminado]

--- src/js/component/ToDoList.jsx
+++ src/js/component/ToDoList.jsx
@@ -5,34 +5,51 @@ const ToDoList = () => {
     const [inputValue, setInputValue] = useState("");
     const [todos, setTodos] = useState([])
 
+    function addTask(inputValue, e) {
+        setTodos(todos.concat(inputValue))
+        e.target.value = ""
+    }
+    function deleteTask(index, e) {
+        setTodos(todos.splice(index, e))
+
+    }
+
     return (
         <div className="container col-6">
             <div className="container">
 
                 <input className="form-control" type="text"
-                    onChange={(e) => { setInputValue(e.target.value) }}
-                    value={inputValue}
-                    onKeyDown={(e) => {
+                    onChange={(e) => {
+                        setInputValue(e.target.value);
+                    }}
+                    //value={inputValue}
+                    onKeyUp={(e) => {
                         e.key === "Enter"
-                            ? setTodos(todos.push(inputValue))
+                            ? addTask(inputValue, e)
                             : null
                     }
                     }
                     placeholder="Que necesitas Hacer?">
-                    {console.log(inputValue)}
-                    {console.log(todos)}
+
                 </input>
 
                 <ul className="list-group">
 
-                    {todos.map((t) => (
-                        <li className="list-group-item disabled" aria-disabled="true">{t}</li>
-                    ))}
-                    <li className="list-group-item">A item</li>
+                    {todos.map((task, index) => (
+                        <li className="list-group-item disabled" aria-disabled="true" key={index}
+                        >{task}  <button type="button" class="btn btn-dark"
+                            onClick={(e) =>
+                                deleteTask(index, e)}
+                        >X</button>
 
+                        </li>
+                    ))}
+                    <div className="text-align-right">
+                        {todos.length} item left
+                    </div>
                 </ul>
             </div>
-        </div>
+        </div >
 
     )
 
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
 	return (
 		<div className="text-center">
 			<h1 className="text-center mt-5">todos</h1>
-			<ToDoList/>
+			<ToDoList />
 		</div>
 	);
 };
